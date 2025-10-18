eb237544bb5bfaa95fd92c7af07746eb8ca3e676
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-10-18 17:22:30 +0200

    fix: build paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
-import { Footer } from "../components/footer";
-import { Header } from "../components/header";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
 import "./globals.css";
 
 const geistSans = Geist({
